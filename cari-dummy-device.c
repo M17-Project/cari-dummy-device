77e7791a50e259e0d3632097c9b3f5535152e486
Wojciech Kaczmarski <w.kaczmarski@teletra.pl>
2025-05-27 09:45:13 +0200

    CARI 1.1 -> 1.3

--- cari-dummy-device.c
+++ cari-dummy-device.c
@@ -1,7 +1,7 @@
 /*
  * cari-dummy-device.c
  *
- *  Edited on: Jan 9, 2025
+ *  Edited on: May 26, 2025
  *  Author: Wojciech Kaczmarski, SP5WWP
  */
 
@@ -15,7 +15,7 @@
 #include "term.h" //colored terminal font
 
 #define DBG_HALT            while(1);
-#define CARI_VER            (((uint8_t)1<<4) | 1)   //CARI v1.1
+#define CARI_VER            (((uint8_t)1<<4) | 3)   //CARI v1.3
 #define CARI_DEV_IDENT      "CARI dummy device, Woj SP5WWP"
 
 typedef enum
@@ -58,6 +58,8 @@ typedef enum
 
 typedef enum
 {
+    SUB_RESET,
+
     SUB_IQ_MOD,
     SUB_RX,
     SUB_TX,
@@ -79,28 +81,33 @@ typedef enum
 
 typedef enum
 {
-    SUB_RX_START,
-    SUB_RX_STOP
+    SUB_ACT_RESET,
+    SUB_ACT_RX_START,
+    SUB_ACT_RX_STOP
 } sub_act_t;
 
 typedef struct
 {
-    uint16_t ctrl_port;
+    uint16_t ul_port, dl_port, ctrl_port, spvn_port;
     int zmq_byte_cnt;
-    void *zmq_ctx, *zmq_dlink, *zmq_meas, *zmq_ulink, *zmq_ctrl;
-    uint8_t ctrl_ok;
+    void *zmq_ctx, *zmq_ul, *zmq_dl, *zmq_ctrl, *zmq_spvn;
+    uint8_t ul_ok, dl_ok, ctrl_ok, spvn_ok;
     uint8_t zmq_buff[1024];
 } cari_t;
 
 //default settings
 cari_t cari =
 {
-    .ctrl_port = 17001,
+    .ul_port = 17000,
+    .dl_port = 17001,
+    .ctrl_port = 17002,
+    .spvn_port = 17003
 };
 
 typedef struct
 {
-    uint64_t        frequency;              //Hz
+    uint64_t        rx_frequency;           //Hz
+    uint64_t        tx_frequency;           //Hz
     float           lna_gain;               //dB
     float           power;                  //dBm
     float           ch_width;               //Hz
@@ -112,6 +119,7 @@ typedef struct
 
 typedef struct
 {
+    char ident[128];
     uint8_t cari_version;
     uint8_t num_capabilities;
     dev_cap_t capabilities[16];
@@ -122,12 +130,13 @@ typedef struct
 //sample settings
 device_t device =
 {
-    CARI_VER,             //CARI version 1.1
+    CARI_DEV_IDENT,
+    CARI_VER,               //CARI version
     1,                      //number of capabilities
     {DEV_SUPERVISION},      //supervision channel available
     2, {
-    {438.8125e6, 0.0f, 30.0f, 12.5e3, 125e3, 0.0f, 5, {SUB_TX, SUB_FM_MOD, SUB_FM_DEMOD, SUB_AFC, SUB_AGC}},
-    {431.2125e6, 0.0f, 30.0f, 12.5e3, 125e3, 0.0f, 5, {SUB_RX, SUB_FM_MOD, SUB_FM_DEMOD, SUB_AFC, SUB_AGC}}}
+    {0.0f, 438.8125e6, 0.0f, 30.0f, 12.5e3, 125e3, 0.0f, 5, {SUB_TX, SUB_FM_MOD, SUB_FM_DEMOD, SUB_AFC, SUB_AGC}},
+    {431.2125e6, 0.0f, 0.0f, 30.0f, 12.5e3, 125e3, 0.0f, 5, {SUB_RX, SUB_FM_MOD, SUB_FM_DEMOD, SUB_AFC, SUB_AGC}}}
 }; 
 
 //debug printf
@@ -152,18 +161,33 @@ void dbg_print(const char* color_code, const char* fmt, ...)
 	}
 }
 
-int cari_init(cari_t *cari)
+void cari_init(cari_t *cari)
 {
-    cari->zmq_ctx = zmq_ctx_new();
-    cari->zmq_dlink = zmq_socket(cari->zmq_ctx, ZMQ_PUB);
-	cari->zmq_meas = zmq_socket(cari->zmq_ctx, ZMQ_PUB);
-	cari->zmq_ulink = zmq_socket(cari->zmq_ctx, ZMQ_SUB);
-    cari->zmq_ctrl = zmq_socket(cari->zmq_ctx, ZMQ_REP);
-
     char addr[128];
-    sprintf(addr, "tcp://*:%d", cari->ctrl_port);
+    int res;
 
-    int res = zmq_bind(cari->zmq_ctrl, addr);
+    cari->zmq_ctx = zmq_ctx_new();
+    cari->zmq_ul = zmq_socket(cari->zmq_ctx, ZMQ_SUB);
+    cari->zmq_dl = zmq_socket(cari->zmq_ctx, ZMQ_PUB);
+    cari->zmq_ctrl = zmq_socket(cari->zmq_ctx, ZMQ_REP);
+	cari->zmq_spvn = zmq_socket(cari->zmq_ctx, ZMQ_PUB);
+
+    //downlink plane init
+    sprintf(addr, "tcp://*:%d", cari->dl_port);
+    res = zmq_bind(cari->zmq_dl, addr);
+
+    if(res)
+    {
+        cari->dl_ok = 0;
+    }
+    else
+    {
+        cari->dl_ok = 1;
+    }
+
+    //control plane init
+    sprintf(addr, "tcp://*:%d", cari->ctrl_port);
+    res = zmq_bind(cari->zmq_ctrl, addr);
 
     if(res)
     {
@@ -174,24 +198,49 @@ int cari_init(cari_t *cari)
         cari->ctrl_ok = 1;
     }
 
-    return res;
+    //supervision plane init
+    sprintf(addr, "tcp://*:%d", cari->spvn_port);
+    res = zmq_bind(cari->zmq_spvn, addr);
+
+    if(res)
+    {
+        cari->spvn_ok = 0;
+    }
+    else
+    {
+        cari->spvn_ok = 1;
+    }   
 }
 
 void dispSettings(device_t *dev, cari_t *cari)
 {
-    uint8_t num_devices = dev->subdevices;
-
     dbg_print(TERM_CLR, "");
 
-    dbg_print(TERM_DEFAULT, "CARI dummy device\n");
+    dbg_print(TERM_BLUE, "**CARI dummy device**\n");
+
+    dbg_print(TERM_DEFAULT, "Ident: ");
+    dbg_print(TERM_YELLOW, "%s\n", dev->ident);
 
     dbg_print(TERM_DEFAULT, "CARI version: ");
     dbg_print(TERM_YELLOW, "%d.%d\n", (dev->cari_version)>>4, (dev->cari_version)&0xF);
 
-    dbg_print(TERM_DEFAULT, "CARI capabilities: ");
+    dbg_print(TERM_DEFAULT, "Capabilities: ");
     for(uint8_t i=0; i<dev->num_capabilities; i++)
         dbg_print(TERM_YELLOW, "%02X " , dev->capabilities[i]);
     dbg_print(TERM_DEFAULT, "\n");
+
+    dbg_print(TERM_DEFAULT, "Subdevices: ");
+    dbg_print(TERM_YELLOW, "%d " , dev->subdevices);
+    dbg_print(TERM_DEFAULT, "\n");
+
+    //zmq planes
+    dbg_print(TERM_DEFAULT, "DL   port: ");
+    dbg_print(TERM_YELLOW, "%d", cari->dl_port);
+    dbg_print(TERM_DEFAULT, ", status ");
+    if(cari->dl_ok)
+        dbg_print(TERM_GREEN, "OK\n");
+    else
+        dbg_print(TERM_YELLOW, "ERROR\n");
 
     dbg_print(TERM_DEFAULT, "CTRL port: ");
     dbg_print(TERM_YELLOW, "%d", cari->ctrl_port);
@@ -199,7 +248,19 @@ void dispSettings(device_t *dev, cari_t *cari)
     if(cari->ctrl_ok)
         dbg_print(TERM_GREEN, "OK\n");
     else
-        dbg_print(TERM_YELLOW, "IDLE\n");
+        dbg_print(TERM_YELLOW, "ERROR\n");
+
+    dbg_print(TERM_DEFAULT, "SPVN port: ");
+    dbg_print(TERM_YELLOW, "%d", cari->spvn_port);
+    dbg_print(TERM_DEFAULT, ", status ");
+    if(cari->spvn_ok)
+        dbg_print(TERM_GREEN, "OK\n");
+    else
+        dbg_print(TERM_YELLOW, "ERROR\n");
+    printf("\n");
+
+    //device info
+    uint8_t num_devices = dev->subdevices;
 
     for(uint8_t i=0; i<num_devices; i++)
     {
@@ -224,7 +285,7 @@ void dispSettings(device_t *dev, cari_t *cari)
 
     for(uint8_t i=0; i<num_devices; i++)
     {
-        char line[100]; memset(line, 0, 100);
+        char line[100] = {0};
         sprintf(line, "| Capabilities: ");
         for(uint8_t j=0; j<dev->subdevice[i].num_capabilities; j++)
             sprintf(&line[strlen(line)], "%02X ", dev->subdevice[i].capabilities[j]);
@@ -238,8 +299,8 @@ void dispSettings(device_t *dev, cari_t *cari)
 
     for(uint8_t i=0; i<num_devices; i++)
     {
-        char line[100]; memset(line, 0, 100);
-        sprintf(line, "| Frequency: %lu Hz", dev->subdevice[i].frequency);
+        char line[100] = {0};
+        sprintf(line, "| RX frequency: %lu Hz", dev->subdevice[i].rx_frequency);
         for(uint8_t j=strlen(line); j<39; j++)
             line[j]=' ';
         sprintf(&line[strlen(line)], "|");
@@ -250,7 +311,19 @@ void dispSettings(device_t *dev, cari_t *cari)
 
     for(uint8_t i=0; i<num_devices; i++)
     {
-        char line[100]; memset(line, 0, 100);
+        char line[100] = {0};
+        sprintf(line, "| TX frequency: %lu Hz", dev->subdevice[i].tx_frequency);
+        for(uint8_t j=strlen(line); j<39; j++)
+            line[j]=' ';
+        sprintf(&line[strlen(line)], "|");
+
+        dbg_print(TERM_DEFAULT, "%s", line);
+    }
+    printf("\n");    
+
+    for(uint8_t i=0; i<num_devices; i++)
+    {
+        char line[100] = {0};
         sprintf(line, "| LNA gain: %2.2f dB", dev->subdevice[i].lna_gain);
         for(uint8_t j=strlen(line); j<39; j++)
             line[j]=' ';
@@ -262,7 +335,7 @@ void dispSettings(device_t *dev, cari_t *cari)
 
     for(uint8_t i=0; i<num_devices; i++)
     {
-        char line[100]; memset(line, 0, 100);
+        char line[100] = {0};
         sprintf(line, "| Power: %2.2f dBm", dev->subdevice[i].power);
         for(uint8_t j=strlen(line); j<39; j++)
             line[j]=' ';
@@ -274,7 +347,7 @@ void dispSettings(device_t *dev, cari_t *cari)
 
     for(uint8_t i=0; i<num_devices; i++)
     {
-        char line[100]; memset(line, 0, 100);
+        char line[100] = {0};
         sprintf(line, "| Channel width: %6.0f Hz", dev->subdevice[i].ch_width);
         for(uint8_t j=strlen(line); j<39; j++)
             line[j]=' ';
@@ -286,7 +359,7 @@ void dispSettings(device_t *dev, cari_t *cari)
 
     for(uint8_t i=0; i<num_devices; i++)
     {
-        char line[100]; memset(line, 0, 100);
+        char line[100] = {0};
         sprintf(line, "| Sample rate: %6.0f Hz", dev->subdevice[i].samp_rate);
         for(uint8_t j=strlen(line); j<39; j++)
             line[j]=' ';
@@ -298,7 +371,7 @@ void dispSettings(device_t *dev, cari_t *cari)
 
     for(uint8_t i=0; i<num_devices; i++)
     {
-        char line[100]; memset(line, 0, 100);
+        char line[100] = {0};
         sprintf(line, "| Frequency correction: %2.2f ppm", dev->subdevice[i].f_corr);
         for(uint8_t j=strlen(line); j<39; j++)
             line[j]=' ';
@@ -311,7 +384,7 @@ void dispSettings(device_t *dev, cari_t *cari)
     dbg_print(TERM_DEFAULT, "--------------------------------------------------------------------------------\n");
 
     dbg_print(TERM_DEFAULT, "Last CARI data: ");
-    for(uint8_t i=0; i<cari->zmq_byte_cnt && i<8; i++)
+    for(uint8_t i=0; i<cari->zmq_byte_cnt && i<16; i++)
         dbg_print(TERM_YELLOW, "%02X ", cari->zmq_buff[i]);
     dbg_print(TERM_DEFAULT, "\n");
 }
@@ -343,13 +416,13 @@ void cari_reply_addr(void *cari_ctrl, cid_t cid, uint8_t addr, uint8_t *params, 
 //the len parameter is the total length
 void cari_reply_noaddr(void *cari_ctrl, cid_t cid, uint8_t *params, uint8_t len)
 {
-    uint8_t rep[len];
+    uint8_t rep[len+3];
 
     rep[0]=cid;
-    *((uint16_t*)&rep[1])=len;
-    memcpy(&rep[3], params, len-3);
+    *((uint16_t*)&rep[1])=len+3;
+    memcpy(&rep[3], params, len);
 
-    zmq_send(cari_ctrl, rep, len, ZMQ_DONTWAIT);    
+    zmq_send(cari_ctrl, rep, len+3, ZMQ_DONTWAIT);    
 }
 
 int main(void)
@@ -363,7 +436,7 @@ int main(void)
         //simple ACK echo for the CTRL plane
         cari.zmq_byte_cnt = zmq_recv(cari.zmq_ctrl, cari.zmq_buff, sizeof(cari.zmq_buff), ZMQ_DONTWAIT);
 
-        if(cari.zmq_byte_cnt>0 /*&& cari.zmq_byte_cnt==*((uint16_t*)&cari.zmq_buff[1])*/)
+        if(cari.zmq_byte_cnt>0 && cari.zmq_byte_cnt==*((uint16_t*)&cari.zmq_buff[1]))
         {
             uint8_t cid = cari.zmq_buff[0];
 
@@ -372,21 +445,33 @@ int main(void)
                 cari_pong(cari.zmq_ctrl, 0);
             }
 
+            else if(cid==CMD_DEV_SET_REG)
+            {
+                uint8_t resp = CARI_OK;
+                cari_reply_noaddr(cari.zmq_ctrl, cid, &resp, 1);
+            }
+
             else if(cid==CMD_DEV_GET_IDENT)
             {
-                cari_reply_noaddr(cari.zmq_ctrl, CMD_DEV_GET_IDENT, (uint8_t*)CARI_DEV_IDENT, strlen(CARI_DEV_IDENT)+3);
+                cari_reply_noaddr(cari.zmq_ctrl, cid, (uint8_t*)device.ident, strlen(device.ident));
             }
 
             else if(cid==CMD_DEV_GET_REG)
             {
-                uint8_t resp = CARI_VER;
-                cari_reply_noaddr(cari.zmq_ctrl, CMD_DEV_GET_REG, &resp, 4);
+                uint8_t addr = cari.zmq_buff[3];
+                uint8_t resp;
+
+                if(addr==0)
+                {
+                    resp = CARI_VER;
+                    cari_reply_noaddr(cari.zmq_ctrl, cid, &resp, 1);
+                }
             }
 
             else //unrecognized command
             {
                 uint8_t err = CARI_UNSUPPORTED;
-                cari_reply_noaddr(cari.zmq_ctrl, cid, &err, 4);
+                cari_reply_noaddr(cari.zmq_ctrl, cid, &err, 1);
             }
 
             dispSettings(&device, &cari);
--- term.h
+++ term.h
@@ -8,6 +8,7 @@
 #pragma once
 
 #define TERM_CLR		"\033c"			//clear terminal
+#define TERM_BLUE       "\033[94m"
 #define TERM_GREEN		"\033[92m"
 #define TERM_RED		"\033[91m"
 #define TERM_YELLOW		"\033[33m"
